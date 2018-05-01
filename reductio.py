91acabacc4f64b8a564570a6436c225f2a54e4b2
BermetKalmakova <bermetkalmakova@gmail.com>
2018-05-01 00:03:12 -0400

    .

--- reductio.py
+++ reductio.py
@@ -7,12 +7,16 @@ def singlefreq(word):
     freq = [i for i in wlist if i.lower() == word.lower()]
     return len(freq)
 
-print "Frequency of nonsense: ", singlefreq("nonsense")
+print "Frequency of darkness: ", singlefreq("darkness")
 print "Frequency of women: ", singlefreq("women")
 print "Frequency of brooklyn: ",singlefreq("brooklyn")
 
 def totalfreq(words):
     return reduce(lambda x,y: x+y, [singlefreq(w) for w in words])
 
-print "Frequency of nonsense and women: ", totalfreq(["nonsense", "women"])
+print "Frequency of darkness and women: ", totalfreq(["nonsense", "women"])
 
+def mostfreq():
+    return reduce(lambda a,b: a if singlefreq(a) > singlefreq(b) else b, wlist)
+
+print mostfreq()
