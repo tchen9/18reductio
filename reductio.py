d6b5824f3a980f03d54ec137b02565254f9aa3bc
Tina <tchen9@stuy.edu>
2018-04-30 23:01:45 -0400

    finished most

--- reductio.py
+++ reductio.py
@@ -1,4 +1,18 @@
 file = open("text.txt","r")
 words = file.read()
 
-print words
+wlist = words.split()
+
+def singlefreq(word):
+    freq = [i for i in wlist if i.lower() == word.lower()]
+    return len(freq)
+
+print "Frequency of nonsense: ", singlefreq("nonsense")
+print "Frequency of women: ", singlefreq("women")
+print "Frequency of brooklyn: ",singlefreq("brooklyn")
+
+def totalfreq(words):
+    return reduce(lambda x,y: x+y, [singlefreq(w) for w in words])
+
+print "Frequency of nonsense and women: ", totalfreq(["nonsense", "women"])
+
